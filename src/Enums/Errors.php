f27a3df4dd2180246a2dc6864472d4a5d7fd3cfd
levan jmukhadze <jmukhadze@gmail.com>
2023-03-30 01:24:24 +0400

    feat: add urgent on send sms; add new service: senderRequest

--- src/Enums/Errors.php
+++ src/Enums/Errors.php
@@ -10,4 +10,9 @@ final class Errors extends Enum
     const INVALID_BRAND_NAME = 101;
     const NOT_ENOUGH_BALANCE = 102;
     const MESSAGE_TOO_LONG = 103;
+    const MESSAGE_ID_NOT_FOUND = 104;
+    const INVALID_NUMBER_FORMAT = 105;
+    const NUMBER_IS_IN_BLACK_LIST = 106;
+    const BRAND_ALREADY_EXISTS = 107;
+    const BRAND_NAME_ADD_IMPOSSIBLE = 108;
 }
